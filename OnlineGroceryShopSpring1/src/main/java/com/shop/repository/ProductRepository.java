17922cd6795581d73a0e6e27ccc6b3c109a2e718
tedivalchanova <teodora.valchanov@gmail.com>
2025-07-11 19:37:08 +0300

    Uploand new task

---
+++ OnlineGroceryShopSpring1/src/main/java/com/shop/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+
+package com.shop.repository;
+
+import com.shop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByName(String name);
+}
